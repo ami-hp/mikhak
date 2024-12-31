7d74ee88403a267273948c751f3c4c9c223706e6
Ahmadreza Nozaeem <aminz.uiux@gmail.com>
2024-12-31 19:46:03 +0330

    added skew to captcha

--- src/app/services/captcha.service.ts
+++ src/app/services/captcha.service.ts
@@ -9,44 +9,46 @@ export class CaptchaService {
   }
 
   generateCaptcha(): string {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let captcha = '';
-    for (let i = 0; i < 6; i++) {
+    const characters : string = '0123456789';
+    let captcha : string  = '';
+    for (let i : number = 0; i < 6; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
       captcha += characters[randomIndex];
     }
     return captcha;
   }
 
-  private getRandomHexColor() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  private getRandomHexColor(): string {
+    const randomColor : string = Math.floor(Math.random() * 16777215).toString(16);
     return `#${randomColor.padStart(6, '0')}`;
   }
 
   generateCaptchaImage(captchaText: string): string | null {
     if (isPlatformBrowser(this.platformId)) {
-      const canvas = document.createElement('canvas');
+      const canvas: HTMLCanvasElement = document.createElement('canvas');
       canvas.width = 150;
       canvas.height = 40;
-      const ctx = canvas.getContext('2d');
+      const ctx : CanvasRenderingContext2D | null = canvas.getContext('2d');
 
       if (ctx) {
         ctx.fillStyle = '#f0f0f0'; // #dadada
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-
         ctx.font = '30px Arial';
         ctx.fillStyle = `${this.getRandomHexColor()}`;
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
-        ctx.fillText(captchaText, canvas.width / 2, canvas.height / 2);
-
-        for (let i = 0; i < 50; i++) {
-          const x = Math.random() * canvas.width;
-          const y = Math.random() * canvas.height;
+        const angle: number = Math.random() * 0.4 - 0.2;
+        const scaleX: number = Math.random() * 0.5 + 0.75;
+        const scaleY: number = Math.random() * 0.5 + 0.75;
+        ctx.setTransform(scaleX, Math.sin(angle), Math.sin(angle), scaleY, canvas.width / 2, canvas.height / 2);
+        ctx.fillText(captchaText, 0, 0);
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        for (let i: number = 0; i < 50; i++) {
+          const x: number = Math.random() * canvas.width;
+          const y: number = Math.random() * canvas.height;
           ctx.fillRect(x, y, 2, 2);
         }
       }
-
       return canvas.toDataURL('image/png');
     } else {
       return null;
