cc486199ffaaa82d165935b397dbc83b0ee6491c
Ahmadreza Nozaeem <aminz.uiux@gmail.com>
2025-02-03 02:23:21 +0330

    build

--- docs/mikhak/server/angular-app-manifest.mjs
+++ docs/mikhak/server/angular-app-manifest.mjs
@@ -15,10 +15,10 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 2801, hash: '0a7a7e19c5ed917ec98ba81ed6ca3b1e0c0925e4c052709fce5a49edcbdbbde7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1159, hash: '0ffe9ccd5a06f93de7a704cdf0478cc3578efda0d91fdb1ee106f978e48b2eee', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 39334, hash: '0941bd1d66ffe4c25ea5d90033ad939264ce64142681d594cd24a92bec035b95', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 12455, hash: 'b7ca327b4389e4219558a87652348a5ebe06fd25f28934d785cc8d1b0d7969cc', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2801, hash: '500c6518cb81789448f624b3e4c1f1d60dcc57b3fac42e9c4b596f98c35e2efc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1159, hash: '9c37d599adf57c75231343483a2c60707b8416bc2eb4881d671674adf6b5720f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 12455, hash: 'd08ea1a23a3935b1655a288be381add42cd8e905165d674a96c5e8795de1f7b5', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 39664, hash: 'd43f5e02ed2b2fc5e28f01bbea1daec686becd0ad9af2af8060eda2ecc160556', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-A5QH4QQ7.css': {size: 14940, hash: 'Rxr6+sk+OJw', text: () => import('./assets-chunks/styles-A5QH4QQ7_css.mjs').then(m => m.default)}
   },
 };
